2447e1aaf0741304dae63c621347a07685618cc6
Maxwell Hankner <maxwellhankner@gmail.com>
2020-03-26 21:30:07 -0500

    writing to the md file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var inquirer = require("inquirer");
+var fs = require("fs");
 
 // const questions = [
 //     'Title',
@@ -12,13 +13,16 @@ var inquirer = require("inquirer");
 //     'Questions'
 // ];
 
-// function writeToFile(fileName, data) {
 
-// }
 
 function init() {
     inquirer
         .prompt([
+            {
+                type: "input",
+                message: "What is your Github username?",
+                name: "github"
+            },
             {
                 type: "input",
                 message: "What is the title of this repo?",
@@ -31,7 +35,7 @@ function init() {
             },
             {
                 type: "input",
-                message: "Enter a table of contents seperated by cammas.",
+                message: "Enter a table of contents (seperated by cammas without spaces).",
                 name: "contents"
             },
             {
@@ -45,36 +49,48 @@ function init() {
                 name: "usage"
             },
             {
-                type: "checkbox",
+                type: "list",
                 message: "What license is being used?",
                 name: "license",
                 choices: [
                     "MIT",
                     "GPLv3",
-                    "AGPL"
+                    "AGPL",
+                    "None"
                 ]
             },
             {
                 type: "input",
-                message: "Who are the contributors?",
+                message: "Who are the contributors (seperated by cammas without spaces)?",
                 name: "contributors"
             },
             {
                 type: "input",
                 message: "How does the user run tests?",
                 name: "tests"
-            },
-            {
-                type: "input",
-                message: "How does the user run tests?",
-                name: "questions"
-            },
+            }
         ])
-        .then(function({github, title, description, contents, installation, usage, license, contributors, tests, questions}) {
-            console.log(title, description);
-            
+        .then(function({github, title, description, contents, installation, usage, license, contributors, tests}) {
+            var inputArray = [title, description, contents.split(','), installation, usage, license, contributors.split(','), tests];
+            var inputData = `# ${title}\n## ${description}`
+            writeToFile("generated-readme.md", inputData)
         });
+    // Build the data according to the user's input
+    // Call the writeToFile function
+}
 
+function writeToFile(fileName, data) {
+    // write all the data to the file
+    fs.writeFile(fileName, data, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+      
+      });
+      
 }
 
 init();
